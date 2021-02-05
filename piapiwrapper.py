f3176d8f29f27ec8a20c002b58ea27f9e76c45f7
SpectralTheorem <matthew.burtell@yale.edu>
2021-02-05 16:06:39 -0500

    remove redundant functions

--- piapiwrapper.py
+++ piapiwrapper.py
@@ -1,4 +1,3 @@
-# ---
 # jupyter:
 #   jupytext:
 #     text_representation:
@@ -12,13 +11,7 @@
 #     name: python3
 # ---
 
-# +
-# testing
-
-# tests for getMarketData
-
-# len(my_list) == len(set(my_list))  # if true, the set has only unique values
-# -
+# TODO email notification on failure
 
 import requests
 import string 
@@ -41,10 +34,6 @@ def getMarketRespose(market_id):
 # None -> list
 # returns a list of all open markets 
 def getAllOpenMarketIDs():
-    # commenting out so I'm not spamming the API
-    # api_url = "https://www.predictit.org/api/marketdata/all/"
-    # 
-    # response = requests.get(api_url)  # fetch market data
     if(response.status_code != 200):
         print("Please wait...")
         time.sleep(3)
@@ -64,8 +53,6 @@ def getAllOpenMarketIDs():
     return market_id_list
 
 
-# TODO: change to reference the all dataframe so I'm not making a ton of requests
-# 2.7 kB individual market vs 453 kB for all 
 # int -> dataframe
 def getMarketDataFrame(market_id):
     response = getMarketRespose(market_id)  
@@ -73,47 +60,6 @@ def getMarketDataFrame(market_id):
     
     market_df = pd.DataFrame.from_dict(xml_response)
     return market_df
-
-
-# int -> print data 
-def printMarketData(market_id):
-    # market_df = getMarketDataFrame(market_id)  # whole market, including 
-    
-    try:
-        print("Market:\t", market_df["shortName"][0])  # every market has a shortname
-    except KeyError:
-        print("That market doesn't have a key \"shortName\"")
-        return
-    tabspacing = "\t\t"
-    print("name", tabspacing, "Last Close Price")
-    
-    
-    for contract_index in range(0, len(market_df)):
-        all_contracts = market_df["contracts"][contract_index]
-        print(all_contracts["name"], end=tabspacing)
-        print(all_contracts["lastClosePrice"], end="\t")
-        print()  # newline
-
-
-# +
-# int -> print data 
-def getBetterMarketDF(market_id):
-    market_df = getMarketDataFrame(market_id)  # whole market, including 
-    
-    try:
-        print("Market:\t", market_df["shortName"][0])
-    except KeyError:
-        print("That market doesn't have a key \"shortName\"")
-        return
-    tabspacing = "\t\t"
-    print("name", tabspacing, "Last Close Price")
-    
-    
-    for contract_index in range(0, len(market_df)):
-        all_contracts = market_df["contracts"][contract_index]
-        print(all_contracts["name"], end=tabspacing)
-        print(all_contracts["lastClosePrice"], end="\t")
-        print()  # newline
 
 
 # request data for entire market 
@@ -133,12 +79,12 @@ def getAllMarketsResponse():
 
 
 
-# +
 # None -> list of all the open markets
 def getAllMarketsList(market_data=None):
     all_markets_response = getAllMarketsResponse()
     return all_markets_response.json()["markets"]
 
+# fetch data if there's none cached
 def fetchIfNoMarketData(market_data=None):
     if market_data is None:
         print("Now fetching market data")
@@ -146,17 +92,16 @@ def fetchIfNoMarketData(market_data=None):
         return market_data
     else:
         return market_data
-    
-    
-    
+
+# return list of market IDs
 def getIdIndexList( market_data=None):
     market_data = fetchIfNoMarketData(market_data)
     
-    id_index_list = []  # 
-    
+    id_index_list = []  # initialize empty list
+
+    # for all markets
     for mkt_index in range(len(market_data)):
-        id_index_list.append(market_data[mkt_index]["id"])
-        # print(id_index_list)
+        id_index_list.append(market_data[mkt_index]["id"])  # append to list
     return id_index_list
 
 # returns all the contracts of a particular market based on the index
@@ -169,37 +114,33 @@ def getContractsDataFrameFromIndex( mkt_index, market_data=None):
     return market_df
 
 
+# using a market id, retrieve the index
 def getIndexFromMarketId( mkt_id, market_data=None):
     market_data = fetchIfNoMarketData(market_data)
     
     market_index_list = getIdIndexList(market_data=market_data)
     try:
+        
         return market_index_list.index(mkt_id)
     except ValueError:
         print(f"There exists no open contracts with id {mkt_id}.")
         return None
     
+    
 # returns all the contracts of a particular market based on ID
 def getContractsDataFrame( mkt_id, market_data=None):
     market_data = fetchIfNoMarketData(market_data)
-    # use the mkt_id to find the market index
     
-    # TD # market_index_list = getIdIndexList(market_data=market_data)  # obsoleted, delete tests pass for gIndexfmMktId
+    # use the mkt_id to find the market index
     market_index = getIndexFromMarketId(mkt_id, market_data=market_data)
     
-    # TD
-    # try:
-    #     market_df = getContractsDataFrameFromIndex( market_index, market_data=market_data)
-    # except ValueError:
-    #     print(f"There exists no open contracts with id {mkt_id}.")
-    #     return None
-    # /TD
-    
     return getContractsDataFrameFromIndex(market_index, market_data=market_data)
+
 
 # adds the word market to a string; used to label market data for concatanation in contracts DataFrame
 def concatMarketString(string):
     return "market"+string.capitalize()
+
 
 # append "market" in front of each column in a dataframe if it doesn't already have the label
 # df -> df
@@ -209,24 +150,7 @@ def labelMarket(market_df):
     else:
         return market_df.rename(concatMarketString, axis="columns")
 
-# """
-# TODO
-# [ ] prepend the general market data frame with the contract specific dataframe
-# [ ] look at sensible plotting options. Format data in a way that's ammenable to those options
-# [ ] make csv saving job so that I don't need to worry about 
-# """
 
-# -
-
-# TODO 
-#class MarketSnapshot( market_data=):
-    # datetime retrieved
-    # retrieve market data if not passed
-    
-    # put all previously created funcitons here 
-
-
-# +
 # Return a dataframe of all information API provides for one market
 def getRawMarketInfo(mkt_id, market_data=None):
     market_data = fetchIfNoMarketData(market_data)
@@ -241,6 +165,3 @@ def getRawMarketInfo(mkt_id, market_data=None):
     
     # combine dfs
     return market_df.join(contracts_df)
-    
-    
-
